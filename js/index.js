0b2bf7880bdabb3f5a0dea249644800c4e0b8c02
Ladislav Martincik <ladislav.martincik@gmail.com>
2017-03-01 09:58:59 +0100

    Install FB pixel + some custom events

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 jQuery(function(){
   jQuery("a.rozvijime__play-video__link").YouTubePopUp();
   jQuery("a.ficury__playbox__play").YouTubePopUp();
+
+  jQuery("a.rozvijime__play-video__link").click(function(e) {
+    fbq('trackCustom', 'VideoPlay', {
+      type: 'intro'
+    });
+  });
+  jQuery("a.ficury__playbox__play").click(function(e) {
+    fbq('trackCustom', 'VideoPlay', {
+      type: 'features'
+    });
+  });
+
+  jQuery("a.objednavka__vybrat, a.button-objednat").click(function(e) {
+    fbq('trackCustom', 'OrderFormClick');
+  })
 });
 
 var rellax = new Rellax('.ficury__background');
